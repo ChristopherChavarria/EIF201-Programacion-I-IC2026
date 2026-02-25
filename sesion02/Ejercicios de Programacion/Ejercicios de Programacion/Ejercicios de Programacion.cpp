c0b8c4dbdb29381a5e8fbf14949a578c96e07b95
Christopher Chavarria <christopher.chavarria.solorzano@est.una.ac.cr>
2026-02-25 15:11:27 -0600

    Practica 01

---
+++ sesion02/Ejercicios de Programacion/Ejercicios de Programacion/Ejercicios de Programacion.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+
+
+//Estadisiticas de un arreglo
+/*
+Solicite al usuario ingresar 10 numeros enteros y los almacene en un arreglo, una vez ingresados todos los valores
+el programa debera calcular e imprimir los siguientes resultados:
+1- El mayor del arreglo
+2- El valor menor del arreglo
+3- La suma de todos los elementos
+4- El promedio aritmetico
+*/
+int main()
+{
+
+    int numeros[10];
+    int mayor = 0; 
+    int menor = 0;
+    int suma = 0;
+
+    for (int i = 0; i < 10; i++) {
+        cout << "Ingrese 10 numeros enteros: " << endl;
+        cin >> numeros[i];
+
+        if (numero[i] > mayor) {
+
+        }
+    }
+    
+
+
+
+
+
+
+
+
+
+    return 0;
+}
+
